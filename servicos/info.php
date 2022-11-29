7b84f2af36cd73eeadb9e0f776867126210980ba
RdrigoMachado <r.drigomachado@gmail.com>
2022-11-29 14:03:10 -0300

    slide de imagens adicionado

--- servicos/info.php
+++ servicos/info.php
@@ -15,11 +15,35 @@ foreach($informacoes as $informacao)
     echo "<p>" , $informacao ," </p>";
 }
 
+
+echo '<div class="content" style="max-width: 930px">';
+$aux = 0;
 foreach($imagens as $imagem)
 {
-    echo '<img src="', $imagem , '" width="300px">';
-    echo '<br>';
+    if($aux == 0){
+        echo '<img class="mySlides" src="', $imagem , '" style="height: 640px; width: 640px; display: block; margin-left: auto;margin-right: auto;">';
+
+    } else {
+        echo '<img class="mySlides" src="', $imagem , '" style="height: 640px; width: 640px; display: none; margin-left: auto;margin-right: auto;">';
+
+    }
+    $aux++;
 }
+echo '</div>';
+
+echo '<div class="center">';
+echo    '<div class="section">';
+echo        '<div class="buttonSlide" onclick="plusDivs(-1)"> ❮ </div>';
+echo        '<div class="buttonSlide" onclick="plusDivs(1)"> ❯ </div>';
+echo   '</div><br/>';
+for($i = 0; $i < sizeof($imagens); $i++)
+{
+    echo '<div class="pag demo" onclick="currentDiv(' , $i + 1 , ')">' , $i + 1 , '</div>'; 
+
+}
+echo   '</div><br/>';
+
+
 
 
 ?>
